c883c90deea10d63933c0a4c2c573cdfd103f3fd
Sai Kiran Belana <Saikiransanju22@gmail.com>
2025-04-20 14:37:27 -0400

    add social profiles

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import Typewriter from "typewriter-effect";
-import { AiOutlineCamera } from "react-icons/ai";
 
 const HomeBG =
   "https://images.unsplash.com/photo-1724365437529-ae10ac6573e8?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
@@ -64,24 +63,41 @@ function Home() {
                   onInit={(typewriter) => {
                     typewriter
                       .typeString(
-                        "<p> I'm <span class='text-white font-semibold'>Saikiran Belana</span></p>",
+                        "I'm <span class='text-white font-semibold'>Saikiran Belana</span><br />",
                       )
                       .pauseFor(500)
-                      .typeString("<p> - Grad Student at UCONN</p>")
+                      .typeString("- Grad Student at UCONN<br />")
                       .pauseFor(500)
-                      .typeString("<p> - Student Developer at UCONN </p>")
+                      .typeString("- Student Developer at UCONN<br />")
                       .pauseFor(700)
                       .typeString(
-                        "<p> <br />  - My passions include OS, & Embedded Systems</p>",
+                        "- My passions include OS & Embedded Systems<br /> <br />",
                       )
                       .pauseFor(500)
                       .typeString(
-                        "<p> - When I'm not coding, you can find me playing CS:GO </p>",
+                        "> When I'm not coding, you can find me playing OverCooked 2<br />",
                       )
                       .pauseFor(500)
                       .typeString(
-                        "<p> - In my free time, I do street photography. <br /><br /> &nbsp;  Check out my profile on <span><a class='bg-white text-accent' href='https://unsplash.com/@belanasaikiran' target='_blank'>Unsplash  <AiOutlineCamera /> ↗</a></span> </p>",
+                        "> In my free time, I do street photography.<br /><br />",
                       )
+                      .typeString(
+                        `&nbsp;📸 Check out my photos on
+                        <a class='underline text-[#B2E7AE]' href='https://unsplash.com/@belanasaikiran' target='_blank'>Unsplash ↗</a><br /><br />`,
+                      )
+                      .typeString(
+                        `&nbsp;🔗 Let's connect:<br />
+                      `,
+                      )
+
+                      .typeString(
+                        `
+                      <a href='https://linkedin.com/in/belanasaikiran' target='_blank' class='text-blue-400 underline'>  LinkedIn</a> |
+                      <a href='https://github.com/belanasaikiran' target='_blank' class='text-gray-300 underline'>GitHub</a>
+                      `,
+                      )
+
+                      // My Social Media Profiles
 
                       // Add more info from github profile
 
